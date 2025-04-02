31793428fd47cae66bc3c4027c91c14cf3b67e49
nkartikaxioned <kartikn@axioned.com>
2025-04-02 19:29:32 +0530

    inprogress

--- react/src/weather-app/getUserInput.jsx
+++ react/src/weather-app/getUserInput.jsx
@@ -1,11 +1,22 @@
 
 
 export const GetUserInput = ({userInput, setUserInput}) => {
+
+  const handleOnChange =(e)=> {
+    let inputValue = e.target.value;
+    setUserInput(inputValue);
+  }
+
   return (
-    <div className="input-container">
-      <label htmlFor="userInput">Enter City Name:</label>
-      <input type="text" id="userInput" maxLength={20}/>
-      <span className="error-txt"></span>
-    </div>
+    <form action="">
+      <div className="input-container">
+        <label htmlFor="userInput">Enter City Name:</label>
+        <input type="text" id="userInput" maxLength={20} value={userInput} onChange={(e)=> handleOnChange(e)}/>
+        <span className="error-txt"></span>
+      </div>
+      <div className="submit-container">
+
+      </div>
+    </form>
   )
 }
