b116d7c300bf54ed5b1c78864b95387ae460fe08
nkartikaxioned <kartikn@axioned.com>
2025-04-03 18:58:07 +0530

    in progress

--- react/src/weather-app/getUserInput.jsx
+++ react/src/weather-app/getUserInput.jsx
@@ -1,21 +1,36 @@
 
 
 export const GetUserInput = ({userInput, setUserInput}) => {
-
+  const emptyError = "Input cannot be empty";
   const handleOnChange =(e)=> {
     let inputValue = e.target.value;
     setUserInput(inputValue);
   }
 
+  const handleFormSubmit =(e) => {
+    e.preventDefault();
+
+    validateSubmitVlaue(userInput);
+  }
+
+  const validateSubmitVlaue =(userInput)=> {
+    
+    if(!userInput) return false;
+    
+    
+    
+    
+  }
+
   return (
-    <form action="">
+    <form onSubmit={handleFormSubmit} >
       <div className="input-container">
         <label htmlFor="userInput">Enter City Name:</label>
-        <input type="text" id="userInput" maxLength={20} value={userInput} onChange={(e)=> handleOnChange(e)}/>
+        <input type="text" id="userInput" maxLength={20} value={userInput} onChange={(e)=> handleOnChange(e)} autoComplete="off"/>
         <span className="error-txt"></span>
       </div>
       <div className="submit-container">
-
+        <button type="submit">Submit</button>
       </div>
     </form>
   )
